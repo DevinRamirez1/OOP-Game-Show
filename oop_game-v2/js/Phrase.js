f7d98d9a217a6ee0223d6d1a085609010075a269
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-31 18:36:44 -0500

    method added to phrase class
    Added addPhraseToDisplay method to Phrase class to insert phrases into html with proper css class identifiers

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+//class for phrase
+class Phrase {
+    constructor() {
+        this.phrase = phrase.toLocaleLowerCase();
+
+    }
+
+    //method to add phrases to html
+    addPhraseToDisplay() {
+        const phraseSection = document.getElementById('phrase');
+        let phraseLi = document.createElement('li');
+        for (let i = 0; i < this.phrase.length; i++){
+            if (this.phrase[i] != ''){
+                phraseLi.classList.add('hide', 'letter', this.phrase[i]);
+                phraseLi.innerHTML = this.phrase[i];
+            } else {
+                phraseLi.classList.add('space');
+            }
+            phraseSection.appendChild(phraseLi);
+        }
+    }
+}
