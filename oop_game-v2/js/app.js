9c4f8674a2d87c9bf787a0b43364f931e6d2b8e1
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-09-01 05:01:04 -0500

    trying to make keys work

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -4,9 +4,8 @@
 
 //variables for app.js
 const game = new Game();
-const phrase = new Phrase();
-const startButton = document.getElementById('btn_reset');
-const keys = document.getElementsByClassName('key')
+const startButton = document.getElementById('btn__reset');
+let keys = document.getElementsByClassName('key')
 
 //eventlistener for the start button
 startButton.addEventListener('click', e => {
