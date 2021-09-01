7b4bfb4d04a43673db158db5bbafb71858e2a66b
DevinRamirez1 <84420982+DevinRamirez1@users.noreply.github.com>
2021-08-31 20:25:37 -0500

    Code for app.js
    created variables related to app.js. Added eventlisteners for start button and keys

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,4 +1,20 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-addPhraseToDisplay();
+
+//variables for app.js
+const game = new Game();
+const phrase = new Phrase();
+const startButton = document.getElementById('btn_reset');
+const keys = document.getElementsByClassName('key')
+
+//eventlistener for the start button
+startButton.addEventListener('click', e => {
+    game.startGame();
+})
+
+//eventlistener for keys
+keys.addEventListener('click', e => {
+    game.handleInteraction(e.target);
+})
+
